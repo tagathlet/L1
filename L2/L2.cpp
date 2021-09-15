c04436dc3847b7f4ac172608a91157d10cdb1d51
Aleksandr Vinogradov <tagathlet@gmail.com>
2021-09-15 05:07:00 +0300

    Без всякой херни, надеюсь

--- L2/L2.cpp
+++ L2/L2.cpp
@@ -3,18 +3,53 @@
 #define BUF_SIZE 255
 
 
-int last_char_index(int i, char *string) {
-	for (i; i < BUF_SIZE; i++) {
-		if (string[i] == ' ' || string[i] == '\n') {
-			return i - 1;
+int get_word_start(int i, char *string) {
+	while (i < BUF_SIZE) {
+		while (string[i] == ' ') {
+			i++;
+		}
+		if (string[i] != '\n') {
+			return i;
+		}
+		else {
+			return -1;
 		}
 	}
 }
 
-bool symmetrical(int first, int last, char *word) {
-	int current_index = first;
-	for (first - current_index; (first - current_index) < (last - current_index) / 2; first++) {
-		if (word[first] != word[last - first + current_index]) {
+int get_word_end(int i, char* string) {
+	while (string) {
+		if (string[i] == ' ' || string[i] == '\n') {
+			return i - 1;
+		}
+		i++;
+	}
+}
+
+bool check_next_word(int i) {
+	if (i == -1) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+bool check_empty_string(char* string) {
+	int start = get_word_start(0, string);
+	if (check_next_word(start)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool check_symmetrical(int first, int last, char *word) {
+	int centre = (last - first) / 2 + first;
+	int end = last + first;
+	for (int i = first; i < centre; i++) {
+		if (word[i] != word[end - i]) {
 			return false;
 		}
 	}
@@ -22,8 +57,9 @@ bool symmetrical(int first, int last, char *word) {
 }
 
 void print_word(int i, int last, char *string) {
-	for (i; i <= last; i++) {
+	while(i <= last) {
 		putc(string[i], stdout);
+		i++;
 	}
 	putc(' ', stdout);
 }
@@ -31,34 +67,34 @@ void print_word(int i, int last, char *string) {
 int main() {
 	
 	char string[BUF_SIZE] = {};
-	int i = 0;
-	int last = 0;
+	int start = 0;
+	int end = 0;
+	bool next_word = true;
 	bool not_found = true;
 
 	printf("Input string: ");
 	fgets(string, sizeof(string), stdin);
 	printf("Symmetrical words: ");
 
-	for (i = 0; i < sizeof(string); i++) {
-		if (string[i] == ' ') {
-			continue;
+	
+	next_word = check_empty_string(string);
+
+	while (next_word) {
+		start = get_word_start(start, string);
+		end = get_word_end(start, string);
+		if (check_symmetrical(start, end, string)) {
+			not_found = false;
+			print_word(start, end, string);
 		}
-		else if (string[i] == '\n') {
-			break;
-		}
-		else {
-			last = last_char_index(i, string);
-			if (symmetrical(i, last, string)) {
-				not_found = false;
-				print_word(i, last, string);
-			}
-			i = last + 1;
-		}
+		start = get_word_start(end + 1, string);
+		next_word = check_next_word(start);
 	}
 
 	if (not_found) {
 		printf("No symmetric words found");
 	}
+	
+	printf("\n");
 
 	return 0;
 }
